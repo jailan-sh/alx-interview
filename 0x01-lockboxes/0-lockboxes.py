a129c8df9eff1ef234ce05d7054d835d2e6d9743
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-12 01:33:12 +0200

    startup 1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened.
+    return true if all open otherwise false
+    """
+    opened = set()
+
+    def dfs(box):
+        opened.add(box)
+        for k in boxes[box]:
+            if k not in opened:
+                dfs(k)
+    dfs(0)
+
+    return len(boxes) == len(opened)
