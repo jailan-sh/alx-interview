32654093ba3f2a0e2eda8e3e2bbde99b9d535ca1
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-26 02:22:53 +0200

    up

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
 """
-script read stdin and return spacific out put 
+script read stdin and return spacific out put
 """
 import sys
 
 
+def print_metrics(fsize=int, scode=dict) -> str:
+    """
+    function to print totalfilesize, statuscodes in ascending order
+    """
+    print("File size: {}".format(fsize))
+    for code in sorted(scode.keys()):
+        if scode[code] > 0:
+            print("{}: {}".format(code, scode[code]))
+
+
+status_count = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, '405': 0, "500": 0}
 total_size = 0
-status_count = {}
+
 try:
     for i, line in enumerate(sys.stdin, start=1):
-        line = line.split()
-        if not line:
-            continue
-        parts = line.split()
-        if parts != 7:
+        line = line.strip().split()
+        if line != 7:
             continue
         try:
-            file_size = int(line.strip[-1])
-            status_code = int(line.split()[-2])
+            file_size = int(line[-1])
+
+            code = int(line[-2])
         except ValueError:
             pass
-        total_size += 1
-        status_count[status_code] = status_count.get(status_code, 0) + 1
+        total_size += file_size
+        if code in status_count.keys():
+            status_count[code] += 1
 
         if i % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(status_count):
-                print("{}: {}".format(code, status_count[code]))
+            print_metrics(total_size, status_count)
             total_size = 0
-            status_count.clear()
-
 except KeyboardInterrupt:
-    pass
-print("File size: {}".format(total_size))
-for code in sorted(status_count):
-    print("{}: {}".format(code, status_count[code]))
-total_size = 0
-status_count.clear()
+    print_metrics(total_size, status_count)
+    raise
+print_metrics(total_size, status_count)
