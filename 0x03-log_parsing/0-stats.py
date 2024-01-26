e66e81f575a9a0446384c697a3915a4591d9a642
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-26 02:38:57 +0200

    remove enumerate

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,25 +18,25 @@ def print_metrics(fsize=int, scode=dict) -> str:
 status_count = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
                 "404": 0, '405': 0, "500": 0}
 total_size = 0
+count = 0
 
 try:
-    for i, line in enumerate(sys.stdin, start=1):
+    for line in sys.stdin:
         pline = line.strip().split()
-        if pline != 7:
-            continue
+        count += 1
         try:
             file_size = int(pline[-1])
-
             code = pline[-2]
-        except ValueError:
+        except Exception:
             pass
         total_size += file_size
         if code in status_count:
             status_count[code] += 1
 
-        if i % 10 == 0:
+        if count == 10:
             print_metrics(total_size, status_count)
             total_size = 0
+            count = 0
 except KeyboardInterrupt:
     print_metrics(total_size, status_count)
     raise
