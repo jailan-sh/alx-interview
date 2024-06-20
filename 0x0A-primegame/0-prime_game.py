e18a45c833f2e4964ae257465ec7409c39486bf8
jailan-sh <jilan.elsherbiny@gmail.com>
2024-06-21 01:51:30 +0300

    pycodestyle

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-the Prime Game 
+The Prime Game
 """
 
 
 def primes(n):
-    """Return list of prime numbers between 1 and n inclusive
-    """
+    """Return a list of prime numbers between 1 and n inclusive."""
     prime = []
     sieve = [True] * (n + 1)
     for p in range(2, n + 1):
-        if (sieve[p]):
+        if sieve[p]:
             prime.append(p)
-            for i in range(p, n + 1, p):
-                sieve[i] = False
+        for i in range(p, n + 1, p):
+            sieve[i] = False
     return prime
+
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game
+    Determines the winner of the Prime Game.
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
