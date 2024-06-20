3cefa487c30b339e7988b25d64e2c8d00b6291d1
jailan-sh <jilan.elsherbiny@gmail.com>
2024-06-21 01:53:06 +0300

    checker13

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,8 +11,8 @@ def primes(n):
     for p in range(2, n + 1):
         if sieve[p]:
             prime.append(p)
-        for i in range(p, n + 1, p):
-            sieve[i] = False
+            for i in range(p, n + 1, p):
+                sieve[i] = False
     return prime
 
 
